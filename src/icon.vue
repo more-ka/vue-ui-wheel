b2b265b6481e691e367e42a9fbe241f34c62d6d6
more-ka <1640627628@qq.com>
2019-05-17 16:20:39 +0800

    将 svg 代码整理到 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href=`#i-${name}`></use>
+    </svg>
+</template>
+<script>
+    export default {
+        props:['name']
+    }
+</script>
+<style lang="scss">
+    .g-icon {
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
