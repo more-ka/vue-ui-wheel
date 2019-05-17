9211f597f1f8dff99ecd226eace6350b6a10980d
more-ka <1640627628@qq.com>
2019-05-17 15:58:04 +0800

    验证 iconPosition 的值是否为 left 或 right 中的一个

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,8 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
         <svg v-if="icon" class="icon">
-        <use :xlink:href=`#i-${icon}`></use>
-    </svg>
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
         <div class="content">
             <slot></slot>
         </div>
@@ -10,29 +10,56 @@
 </template>
 <script>
     export default {
-        props:['icon','iconPosition']
+        //props:['icon','iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
-    .g-button{
+    .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
-        display:inline-flex;
+        display: inline-flex;
         justify-content: center;
         align-items: center;
         border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
         background: var(--button-bg);
         vertical-align: middle;
-        >.content{order:2;}
-        >.icon{order:1;margin-right: .4em;}
-        &:hover{border-color:var(--border-color-hover);}
-        &:active{background-color: var(--button-active-bg);}
-        &:focus{outline:none;}
-        &.icon-right{
-            >.content{order:1;}
-            >.icon{order:2;margin-right: 0;margin-left: .4em;}
+        > .content {
+            order: 2;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .4em;
+        }
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .4em;
+            }
         }
     }
 
