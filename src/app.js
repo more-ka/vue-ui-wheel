ce6211efc2172b0aa62f0d964a406cc043b43bf6
more-ka <1640627628@qq.com>
2019-05-21 16:00:04 +0800

    添加四个单元测试

--- src/app.js
+++ src/app.js
@@ -19,15 +19,74 @@ new Vue({
 
 import chai from 'chai'
 const expect = chai.expect
+//  svg
+{
+    const div = document.createElement('div')
+    document.querySelector('#app').appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new  constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    button.$mount(div)
+    let use = button.$el.querySelector('use')
+    let href = use.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    button.$el.remove()
+    button.$destroy()
+}
+// loading
+{
+    const div = document.createElement('div')
+    document.querySelector('#app').appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new  constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    })
+    button.$mount(div)
+    let use = button.$el.querySelector('use')
+    let href = use.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+//  正常状态,svg order为1
+{
+    const div = document.createElement('div')
+    document.querySelector('#app').appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new  constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
 
-const constructor = Vue.extend(Button)
-const button = new  constructor({
-    propsData:{
-        icon:'settings'
-    }
-})
-button.$mount('#test')
-let use = button.$el.querySelector('use')
-let href = use.getAttribute('xlink:href')
-expect(href).to.eq('#i-settings')
-console.log(href);
+//  loading 状态,svg order为2
+{
+    const div = document.createElement('div')
+    document.querySelector('#app').appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new  constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
