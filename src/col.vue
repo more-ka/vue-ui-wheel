4db05a5ab110803bba31cfb63e8190c0dfb5d739
more-ka <1640627628@qq.com>
2019-05-30 09:42:55 +0800

    row col 的基本功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border: 1px solid red">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -8,7 +11,16 @@
         name:'GuLuCol',
         props:{
             span:{
-                type:[Number,String]}
+                type:[Number,String]
+            },
+            offset:{
+                offset:[Number,String]
+            }
+        },
+        data(){
+            return{
+                gutter:0
+            }
         }
     }
 </script>
@@ -16,12 +28,17 @@
     .col{
         width:50%;
         height: 100%;
-        background: lightblue;
         border: 1px solid lightslategrey;
         $class: col-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
                 width: ($n/24)*100%;
+            }
+        }
+        $class: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 <script>
     export default {
-        name:'GuLuRow'
+        name:'GuLuRow',
+        props:{
+            gutter:{
+                type: [Number,String]
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 <style lang="scss" scoped>
