9258b8e2b6153efcf018f8b5a4a7422ad3c265e9
more-ka <1640627628@qq.com>
2019-05-31 08:14:52 +0800

    响应式基本完成 移动端优先

--- src/col.vue
+++ src/col.vue
@@ -71,7 +71,7 @@
                 margin-left: ($n/24)*100%;
             }
         }
-        @media(min-width:576px) and (max-width:768px){
+        @media(min-width:577px){
         $class: col-ipad-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
@@ -85,7 +85,7 @@
                 }
             }
         }
-        @media(min-width:769px) and (max-width:992px){
+        @media(min-width:769px){
         $class: col-narrowPc-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
