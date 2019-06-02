0d6687a4db23af0132e78347c1aacb6327688a2a
more-ka <1640627628@qq.com>
2019-06-02 21:57:17 +0800

    添加 toast plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './Header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -22,7 +24,8 @@ Vue.component('g-header',Header)
 Vue.component('g-sider',Sider)
 Vue.component('g-content',Content)
 Vue.component('g-footer',Footer)
-
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 new Vue({
     el: '#app',
@@ -32,8 +35,8 @@ new Vue({
         message: 'hi',
     },
     methods:{
-        inputChange(event){
-            console.log(event.target.value);
+        showToast(){
+            this.$toast('我是 toast')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuLuToast'
+    }
+</script>
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height:40px;
+    $toast-bg:rgba(0,0,0,0.74);
+    .toast{
+        position: fixed;
+        top:0;
+        left:50%;
+        transition: translateX(-50%);
+        color: white;
+        padding: 0 16px;
+        height: $toast-height;
+        font-style: $font-size;
+        display: flex;
+        align-items: center;
+        background:$toast-bg;
+        box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.50);
+    }
+</style>
