66ea7521fe6e8cc3cbb5181d58078af60c33f9ef
more-ka <1640627628@qq.com>
2019-06-11 14:52:21 +0800

    实现最基本popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click="onClick">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="onClick">
+        <div class="content-wrapper" v-if="visible"  @click.stop>
             <slot name="content"></slot>
         </div>
         <slot></slot>
@@ -8,32 +8,43 @@
 </template>
 
 <script>
-    export default {
-        name: "GuLuPopover",
-        data(){
-            return {visible:false}
-        },
-        methods:{
-            onClick(){
-                this.visible = !this.visible
+export default {
+  name: "GuLuPopover",
+  data() {
+    return { visible: false };
+  },
+  methods: {
+    onClick() {
+      this.visible = !this.visible;
+      console.log("改变visible的值");
+      if (this.visible === true) {
+        setTimeout(() => {
+            let eventHandler = ()=>{
+                this.visible = false;
+                console.log('点击document关闭popover')
+                document.removeEventListener('click',eventHandler)
             }
-        }
+            document.addEventListener("click", eventHandler);
+        });
+      }
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .popover{
-        display: inline-block;
-        vertical-align: top;
-        margin: 200px;
-        position: relative;
-        >.content-wrapper{
-            position: absolute;
-            bottom: 120%;
-            left: 0;
-            border: 1px solid #eee;
-            padding: 8px;
-            box-shadow: 0 0 3px rgba(0,0,0,0.25);
-        }
-    }
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  margin: 200px;
+  position: relative;
+  > .content-wrapper {
+    position: absolute;
+    bottom: 120%;
+    left: 0;
+    border: 1px solid #eee;
+    padding: 8px;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  }
+}
 </style>
