ef834f3ebbb8c412c00a71b2d1be2c63d1fb412a
more-ka <1640627628@qq.com>
2019-06-11 10:23:09 +0800

    添加 popover 组件

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import TabsItem from './tabs-item'
 import TabsBody from './tabs-body'
 import TabsPane from './tabs-pane'
 import TabsHead from './tabs-head'
+import Popover from './popover'
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
@@ -35,6 +36,7 @@ Vue.component('g-tabs-head',TabsHead);
 Vue.component('g-tabs-body',TabsBody);
 Vue.component('g-tabs-item',TabsItem);
 Vue.component('g-tabs-pane',TabsPane);
+Vue.component('g-popover',Popover);
 
 Vue.use(plugin);
 
---
+++ src/popover.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="popover" @click="onClick">
+        <div class="content-wrapper" v-if="visible">
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuLuPopover",
+        data(){
+            return {visible:false}
+        },
+        methods:{
+            onClick(){
+                this.visible = !this.visible
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .popover{
+        display: inline-block;
+        vertical-align: top;
+        margin: 200px;
+        position: relative;
+        >.content-wrapper{
+            position: absolute;
+            bottom: 120%;
+            left: 0;
+            border: 1px solid #eee;
+            padding: 8px;
+            box-shadow: 0 0 3px rgba(0,0,0,0.25);
+        }
+    }
+</style>
