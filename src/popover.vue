fcdb66994bb6867aa59c110f0d0344940f2a38ac
more-ka <1640627628@qq.com>
2019-06-11 23:07:20 +0800

    解决如果存在overflow hidden造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="onClick">
-        <div class="content-wrapper" v-if="visible"  @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible"  @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <div ref="buttonWrapper">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -16,12 +18,14 @@ export default {
   methods: {
     onClick() {
       this.visible = !this.visible;
-      console.log("改变visible的值");
       if (this.visible === true) {
         setTimeout(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {left,top} = this.$refs.buttonWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left+window.scrollX+'px'
+            this.$refs.contentWrapper.style.top = top+window.scrollY+'px'
             let eventHandler = ()=>{
                 this.visible = false;
-                console.log('点击document关闭popover')
                 document.removeEventListener('click',eventHandler)
             }
             document.addEventListener("click", eventHandler);
@@ -38,13 +42,13 @@ export default {
   vertical-align: top;
   margin: 200px;
   position: relative;
-  > .content-wrapper {
+}
+.content-wrapper {
     position: absolute;
-    bottom: 120%;
     left: 0;
     border: 1px solid #eee;
     padding: 8px;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-  }
+    transform: translateY(-100%);
 }
 </style>
